e6ed271827d30f243c8d46edbf72b96f8a224f96
Groddengrodder <paulblumenthal1@gmail.com>
2024-07-22 14:57:51 +0200

    first changes

---
+++ lua/custom/init.lua
@@ -0,0 +1,7 @@
+require 'custom.settings.autocommands'
+require 'custom.settings.remap'
+require 'custom.settings.options'
+require 'custom.plugins.lazy'
+require 'custom.plugins.rose'
+
+vim.cmd 'colorscheme rose-pine'
---
+++ lua/custom/settings/remap.lua
@@ -0,0 +1,26 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local map = vim.keymap.set
+
+map('n', '<leader>e', vim.cmd.Ex)
+
+map('n', '<leader> w', vim.cmd.w)
+map('n', '<leader> q', vim.cmd.q)
+map('n', '<leader>wq', vim.cmd.wq)
+map('n', '<leader>so', vim.cmd.so)
+
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Diagnostic keymaps
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+--  See `:help wincmd` for a list of all window commands
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
