f56f2e2d63042cbb5ae1e16930d41ed77b74a6fb
Groddengrodder <paulblumenthal1@gmail.com>
2024-08-15 05:35:19 +0200

    da muss noch viel gemacht werden

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,5 +3,6 @@ require 'custom.settings.remap'
 require 'custom.settings.options'
 require 'custom.plugins.lazy'
 require 'custom.plugins.rose'
+require 'custom.plugins.harpoon'
 
 vim.cmd 'colorscheme rose-pine'
--- lua/custom/plugins/lazy.lua
+++ lua/custom/plugins/lazy.lua
@@ -568,6 +568,8 @@ require('lazy').setup({
 
     { 'rose-pine/neovim', name = 'rose-pine', priority = 1000 },
 
+    { 'ThePrimeagen/harpoon', branch = 'harpoon2', dependencies = { 'nvim-lua/plenary.nvim' } },
+
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
